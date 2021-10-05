f768c347a68c5654f9daf8cb6f82774a8cf504e6
Francesco Delle Piane <francescotomae@gmail.com>
2021-10-05 09:47:08 +0200

    Nuova Navbar e Homepage da Aggiustare

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     @endif
 
         <div class="container-fluid home-header justify-content-center">
-            <div class="row justify-content-center align-items-center welcome">
+            <div class="row justify-content-center welcome">
                 <div class="col-12 col-md-6 content">
                     <h1 class="text-center my-5 display-4" id="benvenuti">Benvenuto/a, </h1>
                     <h2>Cosa stai cercando oggi?</h2>
