7e4d41fa9afff2361cfd329fbddc80c598b858b6
Mohamed Wadih <wadihmomo1@gmail.com>
2021-10-05 15:50:48 +0200

    Finished Detail

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,7 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $categories = Category::all();
-        View::share('categories', $categories);
+        if(Schema::hasTable('categories')){
+            $categories = Category::all();
+            View::share('categories', $categories);
+        }
     }
 }
