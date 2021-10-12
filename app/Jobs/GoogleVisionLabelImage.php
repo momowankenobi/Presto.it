3c6c9f996f0b821fdc2c0e7058d09f11fc6f21e5
Mohamed Wadih <wadihmomo1@gmail.com>
2021-10-12 22:56:57 +0200

    merge

--- app/Jobs/GoogleVisionLabelImage.php
+++ app/Jobs/GoogleVisionLabelImage.php
@@ -4,11 +4,12 @@ namespace App\Jobs;
 
 use App\Models\Images;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 
 class GoogleVisionLabelImage implements ShouldQueue
 {
